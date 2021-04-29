af69fa1f8a4d76e706016aa88ede0cfb387c7092
JadsyHB <3291@holbertonschool.com>
2021-04-29 14:11:43 +0000

    initial commit

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-from sys import argv
 from calculator_1 import add, sub, mul, div
+from sys import argv
 
 if __name__ == "__main__":
     if len(argv) != 4:
         print("Usage:", argv[0], "<a> <operator> <b>")
         exit(1)
-    
-    operator = argv[2]
+
+    op = argv[2]
     f = {"+": add, "-": sub, "*": mul, "/": div}
-    if operator not in f:
+    if op not in f:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
 
     a = int(argv[1])
-    b = int(argv[2])
-    print("{:d} {:s} {:d} = {:d}".format(a, op, b, f[operator](a, b)))
+    b = int(argv[3])
+    print("{:d} {:s} {:d} = {:d}".format(a, op, b, f[op](a, b)))
