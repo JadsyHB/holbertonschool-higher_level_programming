dec5f1b69608349c910fdc3098947ba4b524a503
JadsyHB <3291@holbertonschool.com>
2021-05-04 15:11:27 +0000

    Modifying

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -5,4 +5,16 @@ def roman_to_int(roman_string):
     elif roman_string == "":
         return 0
     else:
-        
+        total = 0
+        dic = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+        for i in range(0, len(roman_string)-1):
+            char1 = roman_string[i]
+            char2 = roman_string[i+1]
+            if char1 not in dic.keys():
+                return 0
+            elif dic[char1] < dic[char2]:
+                total -= dic[char1]
+            else:
+                total += dic[char1]
+        total += dic[roman_string[-1]]
+        return total
