bea91ba17e7b5f75d2f71257890eef78e81178e0
JadsyHB <3291@holbertonschool.com>
2021-05-05 18:26:57 +0000

    Fix

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     if type(roman_string) is not str or roman_string is None:
-        return None
-    if roman_string == "":
         return 0
+    if roman_string == "":
+        return None
     total = 0
     dic = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     for i in range(0, len(roman_string)-1):
