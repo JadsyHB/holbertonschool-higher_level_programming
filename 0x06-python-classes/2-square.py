df18ec2c83ac7f96726a9ccb1ca688ec3fcace2d
JadsyHB <3291@holbertonschool.com>
2021-05-11 16:13:51 +0000

    PEP fix

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -18,9 +18,9 @@ class Square:
         Attributes:
             size: size of side of square, default value is 0
         """
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        elif type(size) is not int:
+        if type(size) is not int:
             raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
         else:
             self.__size = size
