6f9d1290a80a2789f6a6603485503f89ebdcbf5b
JadsyHB <3291@holbertonschool.com>
2021-05-14 13:31:06 +0000

    pep8 validation

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -4,7 +4,6 @@ Module 5-text_indentation
 """
 
 
-
 def text_indentation(text):
     """
     prints a text with 2 new lines after ? : . characters
@@ -13,7 +12,7 @@ def text_indentation(text):
         raise TypeError("text must be a string")
     else:
         for char in ".?:":
-            text = text.replace(char, char +"\n\n")
+            text = text.replace(char, char + "\n\n")
         list_text = [line.strip(' ') for line in text.split('\n')]
         new_text = "\n".join(list_text)
         print(new_text, end="")
