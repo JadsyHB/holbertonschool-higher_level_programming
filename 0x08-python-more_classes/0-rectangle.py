285a136ddf3b0f56fe9ceb410553de5b2da648d4
JadsyHB <3291@holbertonschool.com>
2021-05-17 00:26:48 +0000

    initial commit

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Module 0-rectangle
+"""
+
+
+class Rectangle:
+    """
+    empty Rectangle class
+    """
+    pass
---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python3
+"""
+Module 1-rectangle
+"""
+
+
+class Rectangle:
+    """
+    Defines class rectangle with width and height
+    Args:
+    width and height
+    Functions:
+    __init__(self, width, height)
+    width(self)
+    width(self, value)
+    height(self)
+    height(self, value)
+    """
+    @property
+    def width(self):
+        """
+        Getter:
+        returns width
+        """
+        return __width
+
+    @width.setter
+    def width(self, value):
+        """
+        sets width to the value
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """
+        Getter:
+        Returns height
+        """
+        return __height
+
+    @height.setter
+    def height(self, value):
+        """
+        Setter:
+        sets height to value
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def __init__(self, width=0, height=0):
+        """
+        Initializes rectangle
+        """
+        self.width = width
+        self.height = height
+
+    def area(self):
+        """
+        returns area
+        """
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """
+        returns perimeter
+        """
+        if self.__height == 0 or self__.width == 0:
+            return 0
+        return 2 * self.width + 2 * self.height
