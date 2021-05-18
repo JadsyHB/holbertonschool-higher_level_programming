d7b5e98873b739bff35eb41fa75db03afe236dc3
JadsyHB <3291@holbertonschool.com>
2021-05-18 19:19:13 +0000

    initial commit

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Defines class with no attributes
+can only create first_name instance
+"""
+
+
+class LockedClass:
+    """
+    prevent user from making any other attributes 
+    other than first_name
+    """
+    __slots__ = "first_name"
