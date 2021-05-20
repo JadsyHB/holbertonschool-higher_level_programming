01e362004841acc35a6dd739c8e408a9e0bd7d59
JadsyHB <3291@holbertonschool.com>
2021-05-20 16:38:42 +0000

    M

--- 0x0A-lockboxes/0-lockboxes.py
+++ 0x0A-lockboxes/0-lockboxes.py
@@ -2,20 +2,16 @@
 """
 Lockboxes function
 """
+
+
 def canUnlockAll(boxes):
-    """
-    Returns true if boxes can be unlocked
-    """
-    if not isinstance(boxes, list):
-        return False
-    lista = [i for i in range(1, len(boxes))]
-    indexes = boxes[0]
-    for i in indexes:
-        if not isinstance(i, list):
-            return False
-        for j in boxes[i]:
-            if j not in indexes:
-                indexes += [j]
-    if set(lista).issubset(set(indexes)):
-        return True
-    return False
+    """ determine if all boxes can be opened """
+    for key in range(1, len(boxes) - 1):
+        res = False
+        for index in range(len(boxes)):
+            res = key in boxes[index] and key != index
+            if res:
+                break
+        if res is False:
+            return res
+    return True
