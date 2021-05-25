6912cf2a6a54fc7177a1adcc0db1f075fb5b2ce2
JadsyHB <3219@holbertonschool.com>
2021-05-25 15:49:33 +0000

    initial commit

--- 0x0C-python-input_output/14-pascal_triangle.py
+++ 0x0C-python-input_output/14-pascal_triangle.py
@@ -13,10 +13,10 @@ def pascal_triangle(n):
         return pascal
     else:
         for row in range(n+1):
-        subset = [1]
-        for column in range(1, row):
-            subset.append(pascal[row-1][column-1] + pascal[row-1][column])
-        if row != 0:
-            subset.append(1)
-        pascal.append(subset)
-    return pascal
+            subset = [1]
+            for column in range(1, row):
+                subset.append(pascal[row-1][column-1] + pascal[row-1][column])
+            if row != 0:
+                subset.append(1)
+            pascal.append(subset)
+        return pascal
