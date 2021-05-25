fe9676cec7aa1ee278d0df50b5af269b537a6ed1
JadsyHB <3219@holbertonschool.com>
2021-05-25 15:22:42 +0000

    initial commit

--- 0x0C-python-input_output/9-add_item.py
+++ 0x0C-python-input_output/9-add_item.py
@@ -9,8 +9,8 @@ save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
 try:
-    file = load_from_json_file("add_item.json")
+    file_content = load_from_json_file("add_item.json")
 except FileNotFoundError:
-    file = []
+    file_content = []
 
-save_to_json_file(file + argv[1:], "add_item.json")
+save_to_json_file(file_content + argv[1:], "add_item.json")
