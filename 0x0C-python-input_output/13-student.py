253d3f2dd962dbc5851a9403788cec70fee83a99
JadsyHB <3219@holbertonschool.com>
2021-05-25 16:17:53 +0000

    M

---
+++ 0x0C-python-input_output/13-student.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+Student class module
+"""
+
+
+class Student:
+    """
+    Args:
+    first_name, last_name, age
+    def __init__(self, first_name, last_name, age):
+    def to_json(self):
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        initialization
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        class to json
+        """
+        if attrs is None:
+            return self.__dict__
+        else:
+            dic = {}
+            for elem in attrs:
+                if elem in self.__dict__.keys():
+                    dic[elem] = self.__dict__[elem]
+            return dic
+
+    def reload_from_json(self, json):
+        """
+        replaces attributes of Student from json
+        """
+        for key,value in json.items():
+            setattr(self, key, value)
