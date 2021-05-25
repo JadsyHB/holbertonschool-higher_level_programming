9b7de80d7c153a404d425039d5d66d96d3008428
JadsyHB <3219@holbertonschool.com>
2021-05-25 12:43:23 +0000

    adding file

--- 0x0C-python-input_output/1-number_of_lines.py
+++ 0x0C-python-input_output/1-number_of_lines.py
@@ -9,4 +9,7 @@ def number_of_lines(filename=""):
     returns number of lines
     """
     with open(filename, "r") as f:
-        return len(f.readLines())
+        count = 0
+        for line in f:
+            count += 1
+    return count
