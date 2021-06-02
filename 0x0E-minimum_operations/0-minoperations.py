91ef896f6b2b1e46e700250cb3d948ca9b100086
Marwan Bassam <82117479+JadsyHB@users.noreply.github.com>
2021-06-02 18:50:48 +0300

    pep8 fix

--- 0x0E-minimum_operations/0-minoperations.py
+++ 0x0E-minimum_operations/0-minoperations.py
@@ -2,17 +2,19 @@
 """
 Minimum Operations module
 """
+
+
 def minOperations(n):
     """
     return the fewest amount of operations needed
     """
     if n <= 1:
         return 0
-    elif n%3 != 0 and n%2 !=0:
+    elif n % 3 != 0 and n % 2 !=0:
         return n
-    elif n%3 == 0:
-        return 3 + minOperations(n/3)
-    elif n%2 == 0:
-        return 2 + minOperations(n/2)
+    elif n % 3 == 0:
+        return 3 + minOperations(n / 3)
+    elif n % 2 == 0:
+        return 2 + minOperations(n / 2)
     else:
         return 0
