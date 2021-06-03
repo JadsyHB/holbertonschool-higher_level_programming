eeb0618763a11c9d5d8ba57fc0aaf238729b3875
JadsyHB <3219@holbertonschool.com>
2021-06-03 17:58:21 +0000

    modifying

--- 0x0E-minimum_operations/0-minoperations.py
+++ 0x0E-minimum_operations/0-minoperations.py
@@ -12,9 +12,9 @@ def minOperations(n):
         return 0
     elif n % 3 != 0 and n % 2 != 0:
         return n
-    elif n % 3 == 0:
-        return 3 + minOperations(n // 3)
     elif n % 2 == 0:
         return 2 + minOperations(n // 2)
+    elif n % 3 == 0:
+        return 3 + minOperations(n // 3)
     else:
         return 0
