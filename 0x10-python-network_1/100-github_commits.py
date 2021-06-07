b61ec85b46e6650a7d5a05e7d6f945c1824230a8
JadsyHB <3219@holbertonschool.com>
2021-06-07 19:54:38 +0000

    m

--- 0x10-python-network_1/100-github_commits.py
+++ 0x10-python-network_1/100-github_commits.py
@@ -15,5 +15,5 @@ if __name__ == "__main__":
     r = requests.get(url)
     commits = r.json()
     for i in commits[0:10]:
-        print(commit.get('sha'), end=': ')
-        print(commit.get('commit').get('author').get('name'))
+        print(i.get('sha'), end=': ')
+        print(i.get('commit').get('author').get('name'))
