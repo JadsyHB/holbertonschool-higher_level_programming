43e36da16c5117faabf9524b1e8c3e093700eb68
JadsyHB <3219@holbertonschool.com>
2021-06-07 19:09:54 +0000

    adding module 8

---
+++ 0x10-python-network_1/8-json_api.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+module json api 8
+"""
+
+
+from sys import argv
+import requests
+
+
+if __name__ == "__main__":
+    if len(argv) < 2:
+        value = ""
+    else:
+        value = argv[1]
+    url = "http://0.0.0.0:5000/search_user"
+    values = {"q": letter}
+    r = requests.post(url, data=values)
+    try:
+        req = r.json()
+        if req and "id" in req and "name" in req:
+            print("[{}] {}".format(req["id"], req["name"]))
+        else:
+            print("No result")
+    except ValueError as e:
+        print("Not a valid JSON")
