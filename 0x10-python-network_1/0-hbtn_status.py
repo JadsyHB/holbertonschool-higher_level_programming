1876199893341f1317280836220b769e24715e03
JadsyHB <3219@holbertonschool.com>
2021-06-07 15:33:57 +0000

    adding main

--- 0x10-python-network_1/0-hbtn_status.py
+++ 0x10-python-network_1/0-hbtn_status.py
@@ -7,9 +7,10 @@ Module 0 status
 import urllib.request
 
 
-with urllib.request.urlopen('https://intranet.hbtn.io/status') as r:
-    req = r.read()
-    print('Body response:')
-    print("\t- type: {}".format(type(req)))
-    print("\t- content {}".format(req))
-    print("\t- utf8 content: {}".format(req.decode('utf-8')))
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as r:
+        req = r.read()
+        print('Body response:')
+        print("\t- type: {}".format(type(req)))
+        print("\t- content {}".format(req))
+        print("\t- utf8 content: {}".format(req.decode('utf-8')))
