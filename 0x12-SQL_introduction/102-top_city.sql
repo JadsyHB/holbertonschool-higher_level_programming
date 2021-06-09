b215b4537585c7b9d40ab8447bc7fbbabba2b7a6
JadsyHB <3219@holbertonschool.com>
2021-06-09 16:58:45 +0000

    adding module

--- 0x12-SQL_introduction/102-top_city.sql
+++ 0x12-SQL_introduction/102-top_city.sql
@@ -1,7 +1,3 @@
 -- Module 102
 
-SELECT city, AVG(value) as 'avg_temp'
-FROM temperatures
-ORDER BY avg_temp DESC LIMIT 3
-GROUP BY city
-WHERE `month`=7 OR `month`=8;
+SELECT city, AVG(value) as 'avg_temp' FROM temperatures WHERE `month`=7 OR `month`=8 GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
