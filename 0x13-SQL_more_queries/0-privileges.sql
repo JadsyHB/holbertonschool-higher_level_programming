44e916a7206afcbd51bd314a3a5a6e78800e94b7
JadsyHB <3291@holbertonschool.com>
2021-06-11 17:54:49 +0000

    initial commit

---
+++ 0x13-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,4 @@
+-- Module 0: listing all priviliges
+
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
---
+++ 0x13-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- Module 1: create user
+
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* To 'user_od_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+FUSH PRIVILEGES;
