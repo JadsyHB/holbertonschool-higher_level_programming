c73213ec054d4225881090dd5824bea2845985b6
JadsyHB <3291@holbertonschool.com>
2021-06-11 18:24:07 +0000

    adding modules

---
+++ 0x13-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,5 @@
+-- Module 8: cities of california
+
+SELECT id, name
+FROM cities
+WHERE state_id = (SELECT id FROM states WHERE name="California") GROUP BY id ORDER BY id ASC;
---
+++ 0x13-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,6 @@
+-- Module 9: cities by state join
+
+SELECT cities.id, cities.name, states.name
+FROM cities JOIN states
+WHERE cities.state_id = states.id
+ORDER BY cities.id ASC;
