83e97cd1a97ca0d6badc9638c80f78b779d5353a
JadsyHB <3291@holbertonschool.com>
2021-06-11 17:59:06 +0000

    modified

--- 0x13-SQL_more_queries/1-create_user.sql
+++ 0x13-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 -- Module 1: create user
 
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* To 'user_od_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* To 'user_0d_1'@'localhost';
 FUSH PRIVILEGES;
