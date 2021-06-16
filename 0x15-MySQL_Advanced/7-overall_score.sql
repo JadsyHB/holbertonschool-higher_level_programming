0b6049a3c464fc51f539b35c9e05ff722e1ae508
JadsyHB <3291@holbertonschool.com>
2021-06-16 15:25:55 +0000

    m

--- 0x15-MySQL_Advanced/7-overall_score.sql
+++ 0x15-MySQL_Advanced/7-overall_score.sql
@@ -1,16 +1,16 @@
 -- Module 7
 
-DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+DROP PROCEDURE IF EXISTS ComputeOverallScoreForUser;
 
 DELIMITER //
-CREATE PROCEDURE ComputeAverageScoreForUser( IN user_id INT) BEGIN DECLARE avg_score FLOAT;
+CREATE PROCEDURE ComputeOverallScoreForUser( IN user_id INT) BEGIN DECLARE overall_score FLOAT;
 
-SET avg_score = (
+SET overall_score = (
 SELECT  AVG(score)
 FROM corrections AS C
 WHERE C.user_id=user_id); UPDATE users 
 
-SET average_score = avg_score
+SET overall_score = overall_score
 WHERE id=user_id;
 END // 
 DELIMITER ; 
