debeee53785ca5708ca2ea7781cb3feed69cfdd8
JadsyHB <3291@holbertonschool.com>
2021-06-16 14:51:31 +0000

    m

--- 0x15-MySQL_Advanced/3-glam_rock.sql
+++ 0x15-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,7 @@
 -- Module 3: list all bands with GLAM as their main style
 
-SELECT band_name, (IFNULL(split, 2020) - formed) AS 'lifespan'
+SELECT band_name, IFNULL(split, 2020) - IFNULL(formed, 0) AS 'lifespan'
 FROM metal_bands
 GROUP BY band_name
+WHERE style LIKE "%Glam rock%"
 ORDER BY lifespan DESC;
