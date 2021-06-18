19f38bd86358ee9a0bdd1124f8416c1be9c7ffa6
JadsyHB <3291@holbertonschool.com>
2021-06-18 20:31:57 +0000

    fix error

--- 0x16-count_it/0-count.py
+++ 0x16-count_it/0-count.py
@@ -16,32 +16,32 @@ def count_words(subreddit, word_list, nextPage=None, dic={}):
     else:
         url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
         res = requests.get(url, headers=headers, allow_redirects=False)
-    if res.status_code >= 400:
-        return
+    if res.status_code == 200:
+        titles = []
 
-    titles = []
+        for elem in res.json()['data']['children']:
+            titles.append(elem['data']['title'])
 
-    for elem in res.json()['data']['children']:
-        titles.append(elem['data']['title'])
+        for word in word_list:
+            word = word.lower()
+            for title in titles:
+                title_words = list(map(lambda x: x.lower(), title.split()))
+                if word in title_words:
+                    if word in dic:
+                        dic[word] += title_words.count(word)
+                    else:
+                        dic[word] = title_words.count(word)
 
-    for word in word_list:
-        word = word.lower()
-        for title in titles:
-            title_words = list(map(lambda x: x.lower(), title.split()))
-            if word in title_words:
-                if word in dic:
-                    dic[word] += title_words.count(word)
-                else:
-                    dic[word] = title_words.count(word)
-
-    nextPage = res.json()['data']['after']
-    if nextPage:
-        count_words(subreddit, word_list, nextPage, dic)
+        nextPage = res.json()['data']['after']
+        if nextPage:
+            count_words(subreddit, word_list, nextPage, dic)
+        else:
+            s = {
+                k: v for k, v in sorted(
+                    dic.items(), key=lambda item: item[1], reverse=True
+                    )
+                }
+            for key, value in s.items():
+                print("{}: {}".format(key, value))
     else:
-        s = {
-            k: v for k, v in sorted(
-                dic.items(), key=lambda item: item[1], reverse=True
-                )
-            }
-        for key, value in s.items():
-            print("{}: {}".format(key, value))
+        return
