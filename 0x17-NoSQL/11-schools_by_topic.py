0ee0e73627fa4fa6b1ce87c0a65e9138e33aafae
JadsyHB <3291@holbertonschool.com>
2021-06-22 15:27:49 +0000

    m

---
+++ 0x17-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+module 11
+"""
+
+import pymongo
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    return list
+    """
+    return [i for i in mongo_collection.find({"topic":topic})]
