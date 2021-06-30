5d5e91eed7ae62b622a73e514bd4ce48edfd4927
JadsyHB <3219@holbertonschool.com>
2021-06-30 20:27:03 +0000

    m

--- 0x1A-caching/1-fifo_cache.py
+++ 0x1A-caching/1-fifo_cache.py
@@ -1,34 +1,29 @@
 #!/usr/bin/python3
-"""
-fifocache
-"""
+""" FIFO caching """
 from base_caching import BaseCaching
 
 
 class FIFOCache(BaseCaching):
-    """
-    class FIFO
-    """
+    """ FIFO CACHE"""
     def __init__(self):
-        """init"""
         super().__init__()
         self.data = {}
         self.next_in, self.next_out = 0, 0
-
-    def _push(self, key, item):
-        """FIFO Push"""
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS - 1:
-            print("DISCARD: {}".format(self.data[self.next_out + 1]))
-            self._pop()
-        self.cache_data[key] = item
-        self.next_in += 1
-        self.data[self.next_in] = key
 
     def _pop(self):
         """ FIFO pop"""
         self.next_out += 1
         key = self.data[self.next_out]
         del self.data[self.next_out], self.cache_data[key]
+
+    def _push(self, key, item):
+        """ FIFO push"""
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS - 1:
+            print("DISCARD: {}".format(self.data[self.next_out + 1]))
+            self._pop()
+        self.cache_data[key] = item
+        self.next_in += 1
+        self.data[self.next_in] = key
 
     def put(self, key, item):
         """put"""
