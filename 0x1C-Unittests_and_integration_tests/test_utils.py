7b8292d9abf9aa45ab3aeb15a65b5facce426520
JadsyHB <3219@holbertonschool.com>
2021-07-07 18:27:29 +0000

    added test

--- 0x1C-Unittests_and_integration_tests/test_utils.py
+++ 0x1C-Unittests_and_integration_tests/test_utils.py
@@ -3,7 +3,7 @@
 
 from parameterized import parameterized
 from unittest import TestCase
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 from unittest.mock import patch, Mock
 
 
@@ -44,3 +44,22 @@ class TestGetJson(TestCase):
         with patch('requests.get', return_value=mock_resp):
             resp = get_json(test_url)
             self.assertEqual(resp, test_payload)
+
+class TestMemoize(TestCase):
+    """class for testing  memoize"""
+    def test_memoize(self):
+        """test memoize"""
+        class TestClass:
+
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass, 'a_method', return_value=42) as p:
+            test_class = TestClass()
+            ret = test_class.a_property
+            self.assertEqual(ret, 42)
+            p.assert_called_once()
