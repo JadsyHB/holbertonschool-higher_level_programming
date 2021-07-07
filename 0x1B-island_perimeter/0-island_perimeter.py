035e7e3f5fe7ffe834e9e136b03a2408cda710a9
JadsyHB <3219@holbertonschool.com>
2021-07-07 17:28:53 +0000

    fixed

--- 0x1B-island_perimeter/0-island_perimeter.py
+++ 0x1B-island_perimeter/0-island_perimeter.py
@@ -9,17 +9,21 @@ def island_perimeter(grid):
     island perimeter return perimeter
     """
     perim = 0
-    length = len(grid) - 1
-    width = len(grid[0]) - 1
+    length = len(grid)
+    width = len(grid[0])
     for i in range(length):
         for j in range(width):
             if grid[i][j] == 1:
-                if i == 0 or grid[i - 1][j] != 1:
+                if i == 0 or i == length - 1:
                     perim += 1
-                if j == 0 or grid[i][j - 1] != 1:
+                if j == 0 or j == width - 1:
                     perim += 1
-                if j == width or grid[i][j + 1] != 1:
+                if grid[i-1][j] == 0:
                     perim += 1
-                if i == length or grid[i + 1][j] != 1:
+                if i < length-1 and grid[i+1][j] == 0:
+                    perim += 1
+                if grid[i][j-1] == 0:
+                    perim += 1
+                if j < width-1 and grid[i][j+1] == 0:
                     perim += 1
     return perim
