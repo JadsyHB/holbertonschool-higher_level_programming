ecc6dc0d423619634c68fb1222371eec5ae836fa
JadsyHB <3219@holbertonschool.com>
2021-07-07 17:47:03 +0000

    initial commit

---
+++ 0x1C-Unittests_and_integration_tests/README.md
@@ -0,0 +1,1 @@
+# unittest integration test
---
+++ 0x1C-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""" Unittest module """
+
+from unittest import TestCase
+from utils import access_nested_map
+
+
+class TestAccessNestedMap(TestCase):
+    """
+    unittest test class
+    """
+    @parametrized.expand([
+        ({"a": 1}, ("a",), 1), 
+        ({"a": {"b": 2}}, ("a",), {"b": 2}), 
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+    def test_access_nested_map(self, map, path, expected_out):
+        """Test method for access nested map"""
+        out = access_nested_map(map, path)
+        self.assertEqual(out, expected_out)
